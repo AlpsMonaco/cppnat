8decbeb2760478ace74d373cffb46087a154d63f
kezhengjie <kzj961014@qq.com>
2022-04-09 17:25:46 +0800

    update

--- src/message.h
+++ src/message.h
@@ -3,11 +3,49 @@
 
 namespace cppnat
 {
+	template <int bufferSize, int headerSize>
+	class Buffer
+	{
+	public:
+		Buffer() : data(nullptr)
+		{
+			memset(this->buffer, 0, bufferSize);
+			data = this->buffer + headerSize;
+		}
+		int BufferSize() { return bufferSize; }
+		int HeaderSize() { return headerSize; }
+
+		char *GetBuffer() { return buffer; }
+		char *GetData() { return data; }
+
+	protected:
+		char buffer[bufferSize];
+		char *data;
+	};
+
+	enum class MsgCode : unsigned short
+	{
+		FAILED = 0x00,
+		SUCCESS = 0x01,
+		REMOTE_NOT_CONNECT = 0x02
+	};
+
 	enum class MsgEnum : unsigned short
 	{
-		Success = 1,
+		ECHO = 0x0000,
+		ECHO_VERSION = 0x0001,
 
-		AnotherIsInvalid = 1001,
+		CMD_REQUEST_NAT = 0x0101,
+		CMD_TRANSPORT_DATA = 0x0102,
+	};
+
+	template <typename T>
+	unsigned short GetNumber(T t) { return static_cast<unsigned short>(t); }
+
+	struct RequestNAT
+	{
+		unsigned short port;
+		char ip[16];
 	};
 }
 
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,7 +1,22 @@
 #include "server.h"
+#include <iostream>
+
 using namespace cppnat;
 
-constexpr int BufferSize = 65536;
+constexpr int bufferSize = 65536;
+constexpr int headerSize = 2;
+
+#define LOGINFO(s)                                \
+	do                                            \
+	{                                             \
+		std::cout << "[INFO] " << s << std::endl; \
+	} while (0)
+
+#define LOGERROR(s)                                \
+	do                                             \
+	{                                              \
+		std::cout << "[ERROR] " << s << std::endl; \
+	} while (0)
 
 Server::Server(const char *addr, int port) : stop(false)
 {
@@ -34,7 +49,7 @@ bool Server::Listen()
 		return false;
 	if (bind(this->fd, (sockaddr *)&this->addr, sizeof(this->addr)) == SOCKET_ERROR)
 		return false;
-	if (listen(this->fd, 40) == SOCKET_ERROR)
+	if (listen(this->fd, 10) == SOCKET_ERROR)
 		return false;
 	return true;
 }
@@ -48,6 +63,11 @@ int Server::Errno()
 #endif
 }
 
+/**
+ * @brief a pair contains two sockets
+ * @param fd1 default -1
+ * @param fd2 default -1
+ */
 struct ConnPair
 {
 	ConnPair(int c1 = INVALID_SOCKET, int c2 = INVALID_SOCKET) : c1(c1), c2(c2)
@@ -55,19 +75,38 @@ struct ConnPair
 		memset(&s1, 0, sizeof(s1));
 		memset(&s2, 0, sizeof(s2));
 	}
+	/**
+	 * @brief call this function when one socket is close;
+	 * @param fd socket to be closed
+	 */
+	void Disconnect(int fd)
+	{
+		if (fd == c1)
+			c1 = INVALID_SOCKET;
+		else if (fd == c2)
+			c2 = INVALID_SOCKET;
+	}
 	SOCKET c1;
 	sockaddr_in s1;
 	SOCKET c2;
 	sockaddr_in s2;
 };
 
-inline void Handle(SOCKET &fd, SOCKET &another, FD_SET *pfdset)
+inline void Handle(SOCKET &fd, SOCKET &another, FD_SET *pfdset, ConnPair &connPair)
 {
-	static char buf[BufferSize];
-	int len = recv(fd, buf, BufferSize, 0);
+	static char buf[bufferSize];
+	int len = recv(fd, buf, bufferSize, 0);
 	if (len == 0)
 	{
+		connPair.Disconnect(fd);
+		FD_CLR(fd, pfdset);
+		closesocket(fd);
+		/**
+		 * @todo:send fd is disconnected to another
+		 */
+		return;
 	}
+	send(another, buf, len, 0);
 }
 
 void Server::Begin()
@@ -102,6 +141,7 @@ void Server::Begin()
 				FD_SET(connPair.c1, &fdset);
 				if (connPair.c1 > maxSocket)
 					maxSocket = connPair.c1;
+				LOGINFO(std::string("accept c1"));
 			}
 			else if (connPair.c2 == INVALID_SOCKET)
 			{
@@ -111,6 +151,7 @@ void Server::Begin()
 				FD_SET(connPair.c2, &fdset);
 				if (connPair.c2 > maxSocket)
 					maxSocket = connPair.c2;
+				LOGINFO(std::string("accept c2"));
 			}
 			else
 			{
@@ -119,15 +160,17 @@ void Server::Begin()
 				static socklen_t tempLen;
 				tempFd = accept(this->fd, (sockaddr *)&tempAddr, &tempLen);
 				closesocket(tempFd);
+				send(tempFd, "forbidden", 9, 0);
+				LOGINFO(std::string("invalid client connect"));
 			}
 		}
 		while (count > 0)
 		{
 			count--;
 			if (FD_ISSET(connPair.c1, &rlist))
-				Handle(connPair.c1, connPair.c2, &fdset);
+				Handle(connPair.c1, connPair.c2, &fdset, connPair);
 			else
-				Handle(connPair.c2, connPair.c1, &fdset);
+				Handle(connPair.c2, connPair.c1, &fdset, connPair);
 		}
 	}
 }
