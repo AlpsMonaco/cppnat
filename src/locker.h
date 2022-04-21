be40a44702e150d182383036bb9d0224092b9e6b
kezhengjie <kzj961014@qq.com>
2022-04-21 22:56:05 +0800

    update

--- src/locker.h
+++ src/locker.h
@@ -68,6 +68,67 @@ namespace cppnat
 	protected:
 		LockerItemType *items[size];
 	};
+
+	template <size_t size>
+	class LockerNoVirtual
+	{
+	public:
+		using LockerNoVirtualInstance = LockerNoVirtual<size>;
+		class Proxy
+		{
+		public:
+			Proxy(LockerNoVirtualInstance &locker) : locker(locker) {}
+			Proxy &operator()(size_t index)
+			{
+				this->index = index;
+				return *this;
+			}
+
+			template <typename T>
+			operator T &()
+			{
+				assert(this->index < size);
+				assert(locker.IdMap[this->index] == typeid(T).hash_code());
+				return *reinterpret_cast<T *>(locker.items[this->index]);
+			}
+
+			template <typename T>
+			operator T() = delete;
+
+		protected:
+			LockerNoVirtualInstance &locker;
+			size_t index;
+		};
+
+		using item = void *;
+		LockerNoVirtual() : proxy(*this)
+		{
+			for (size_t i = 0; i < size; i++)
+			{
+				items[i] = nullptr;
+				IdMap[i] = 0;
+			}
+		}
+
+		template <typename T>
+		void Put(size_t index, item p)
+		{
+			assert(index < size);
+			items[index] = p;
+			IdMap[index] = typeid(T).hash_code();
+		}
+
+		Proxy &operator[](size_t index)
+		{
+			assert(index < size);
+			return proxy(index);
+		}
+
+	protected:
+		Proxy proxy;
+		item items[size];
+		size_t IdMap[size];
+	};
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,20 @@ int main()
 	locker.Put<unsigned short>(1, &j);
 	locker.Put<long long>(2, &k);
 
+	LockerNoVirtual<3> lockerNew;
+	lockerNew.Put<int>(0, &i);
+	lockerNew.Put<unsigned short>(1, &j);
+	lockerNew.Put<long long>(2, &k);
+
 	int &ri = locker[0];
 	unsigned short &rj = locker[1];
-	unsigned long long &rk = locker[2];
+	long long &rk = locker[2];
+	// unsigned long long &rk = locker[2];
 	Println(ri, rj, rk);
+
+	int &riNew = lockerNew[0];
+	unsigned short &rjNew = lockerNew[1];
+	long long &rkNew = lockerNew[2];
+	Println(riNew, rjNew, rkNew);
+	// unsigned long long &rkNew = lockerNew[2];
 }
