a2ee349151da678202fcf4214379b8881e4c08d2
kezhengjie <kzj961014@qq.com>
2022-05-17 16:56:42 +0800

    update

--- src/connection.h
+++ src/connection.h
@@ -1,10 +1,60 @@
 #ifndef __CPP_NAT_CONNECTION_H
 #define __CPP_NAT_CONNECTION_H
 #include <vector>
-#include <vector>
+#include <memory>
+#include <functional>
 
 namespace cppnat
 {
+    template <typename Conn, typename Closer, size_t bufferSize>
+    class BufferedConn
+    {
+    public:
+        constexpr static size_t kBufferSize = bufferSize;
+        using ConnId = size_t;
+        using CloseFunction = std::function<void(Conn &)>;
+
+        BufferedConn(ConnId id, Closer &&closeMethod) : id_(id),
+                                                        pConn(nullptr),
+                                                        closeFunction_(std::forward<Closer>(closeMethod))
+        {
+        }
+
+        template <typename... Args>
+        inline void Init(Args &&...args) { pConn_ = std::make_unique<Conn>(std::forward<Args>(args)...); }
+        inline Conn &Conn() { return *pConn_; }
+        inline ConnId Id() { return const_cast<BufferedConn *>(this)->Id(); }
+        inline char *ReadBuffer() { return readBuffer_; }
+        inline char *WriteBuffer() { return writeBuffer_; }
+        inline void Close() { closeFunction_(*pConn_); }
+        inline ConnId Id() const { return id_; }
+        inline const char *ReadBuffer() const { return readBuffer_; }
+        inline const char *WriteBuffer() const { return writeBuffer_; }
+        inline size_t BufferSize() const { return kBufferSize; }
+        inline size_t BufferSize() { return kBufferSize; }
+
+    protected:
+        CloseFunction closeFunction_;
+        ConnId id_;
+        std::unique_ptr<Conn> pConn_;
+        char readBuffer_[kBufferSize];
+        char writeBuffer_[kBufferSize];
+    };
+
+    template <typename Conn, size_t bufferSize, size_t maxSize>
+    class BufferedConnMgr
+    {
+    public:
+        using BufferedConnPtr = std::unique_ptr<
+            BufferedConn<Conn, bufferSize>>;
+
+        BufferedConnMgr() : activeConn_(0) {}
+
+    protected:
+        BufferedConnPtr bufferedConnList_[maxSize];
+        size_t activeConn_;
+    };
+
     template <typename Conn, size_t bufferSize>
     class ConnManager
     {
