b26dff80b3db06967ac851a458668d3289688a71
kezhengjie <kzj961014@qq.com>
2022-08-31 11:39:01 +0800

    update

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -6,32 +6,37 @@
 #include "../log.h"
 #include "../server.h"
 
-int main(int argc, char** argv) {
-  cppnat::Log::SetLogName("nat_server");
-  cppnat::Log::Info("cppnat server start");
-  std::ifstream ifs("server.json");
-  if (!ifs.is_open()) {
-    cppnat::Log::Error("open server.json failed");
-    return 1;
-  }
-  std::string config_file_content((std::istreambuf_iterator<char>(ifs)),
-                                  (std::istreambuf_iterator<char>()));
-  jsr::Json json;
-  auto err = json.Parse(config_file_content);
-  if (err) {
-    cppnat::Log::Error(err.Message());
-    return 1;
-  }
-  std::string ip;
-  std::uint16_t port;
-  err = json.Unmarshal({
-      {"bind_ip", ip},
-      {"bind_port", port},
-  });
-  if (err) {
-    cppnat::Log::Error(err.Message());
-    return 1;
-  }
-  cppnat::Server server(ip, port);
-  server.Start();
+int main(int argc, char** argv)
+{
+    cppnat::Log::SetLogName("nat_server");
+    cppnat::Log::Info("cppnat server start");
+    std::ifstream ifs("server.json");
+    if (!ifs.is_open())
+    {
+        cppnat::Log::Error("open server.json failed");
+        return 1;
+    }
+    std::string config_file_content((std::istreambuf_iterator<char>(ifs)),
+                                    (std::istreambuf_iterator<char>()));
+    jsr::Json json;
+    auto err = json.Parse(config_file_content);
+    if (err)
+    {
+        cppnat::Log::Error(err.Message());
+        return 1;
+    }
+    std::string ip;
+    std::uint16_t port;
+    err = json.Unmarshal({
+        {"bind_ip", ip},
+        {"bind_port", port},
+    });
+    if (err)
+    {
+        cppnat::Log::Error(err.Message());
+        return 1;
+    }
+    cppnat::Log::Info("server listening on {}:{}", ip, port);
+    cppnat::Server server(ip, port);
+    server.Start();
 }
